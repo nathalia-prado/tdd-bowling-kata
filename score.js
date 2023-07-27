e4f3188c98e14c35a5edb4302513c82df5c06855
Nathalia Prado <nathalia_898@hotmail.com>
2023-07-27 20:20:57 +1200

    scoring entire game missed and refactoring

--- score.js
+++ score.js
@@ -2,22 +2,29 @@ export {
     scoreFrame
 }
   
-function scoreFrame (frame, nextFrame, thirdFrame, isTenth) {
-    let score = frame[0] + frame[1]
-    if(isTenth && (isSpare(score) || isStrike(frame))) {
-        score += frame[2]
-    } else {
-        if(isStrike(frame)){
-            if(isStrike(nextFrame)){
-                score += thirdFrame[0] 
+function scoreFrame (frames) {
+    let totalScoresFrames = 0
+    frames.forEach((frame, index) => {
+        const isTenth = index + 1 === frames.length
+        let score = frame[0] + frame[1]
+        if(isTenth && (isSpare(score) || isStrike(frame))) {
+            score += frame[2]
+        } else {
+            const nextFrame = frames[index + 1]
+            if(isStrike(frame)){
+                if(isStrike(nextFrame)){
+                    const thirdFrame = frames[index + 2]
+                    score += thirdFrame[0] 
+                }
+                score += nextFrame[0] + nextFrame[1]
             }
-            score += nextFrame[0] + nextFrame[1]
-        }
-        if(isSpare(score)) {
-            score += nextFrame[0] 
+            if(isSpare(score)) {
+                score += nextFrame[0] 
+            } 
         } 
-    } 
-    return score
+        totalScoresFrames += score
+    })
+    return totalScoresFrames
 }
 
 function isStrike(frame) {
--- score.test.js
+++ score.test.js
@@ -8,7 +8,7 @@ test('test setup working', () => {
 test('scores a gutterball frame', () => {
     const frame = [0, 0]
     const expected = 0
-    const actual = score.scoreFrame(frame)
+    const actual = score.scoreFrame([frame])
   
     expect(actual).toBe(expected)
 })
@@ -16,71 +16,69 @@ test('scores a gutterball frame', () => {
 test('scores a normal frame', () => {
     const frame = [2, 3]
     const expected = 5
-    const actual = score.scoreFrame(frame)
+    const actual = score.scoreFrame([frame])
     expect(actual).toBe(expected)
 })  
 
 test('scores a spare frame', () => {
     const frame = [4, 6]
     const nextFrame = [3, 5]
-    const expected = 13
-    const actual = score.scoreFrame(frame, nextFrame)
+    const expected = 21
+    const actual = score.scoreFrame([frame, nextFrame])
     expect(actual).toBe(expected)
 })  
 
 test('scores a single strike frames', () => {
     const frame = [10, 0]
     const nextFrame = [3, 5]
-    const expected = 18
-    const actual = score.scoreFrame(frame, nextFrame)
+    const expected = 26
+    const actual = score.scoreFrame([frame, nextFrame])
     expect(actual).toBe(expected)
 })  
 
 test('scores a double strike frames', () => {
-    const frame = [10, 0]
+    const frame = [10, 0] 
     const nextFrame = [10, 0]
-    const thirdFrame = [3,4]
-    const expected = 23
-    const actual = score.scoreFrame(frame, nextFrame, thirdFrame)
+    const thirdFrame = [3, 4]
+    const expected = 47
+    const actual = score.scoreFrame([frame, nextFrame, thirdFrame])
     expect(actual).toBe(expected)
 })  
+
+test('scores an entire game', () => {
+    const frames = [[1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [4, 4]]
+    const expected = 119
+    const actual = score.scoreFrame(frames)
+    expect(actual).toBe(expected)
+})  
+
 
 test('scores a normal in the 10th frame', () => {
     const frame = [3, 5]
     const nextFrame = [10, 0]
     const thirdFrame = [3,4]
-    const isTenth = true
-    const expected = 8
-    const actual = score.scoreFrame(frame, nextFrame, thirdFrame, isTenth)
+    const expected = 32
+    const actual = score.scoreFrame([frame, nextFrame, thirdFrame])
     expect(actual).toBe(expected)
 })  
 
 test('scores a spare in the 10th frame', () => {
-    const frame = [6, 4, 6]
-    const nextFrame = [10, 0]
-    const thirdFrame = [3,4]
-    const isTenth = true
-    const expected = 16
-    const actual = score.scoreFrame(frame, nextFrame, thirdFrame, isTenth)
+    const frames = [[1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [6, 4, 6]]
+    const expected = 127
+    const actual = score.scoreFrame(frames)
     expect(actual).toBe(expected)
 })  
 
 test('scores a strike in the 10th frame', () => {
-    const frame = [10, 0, 6]
-    const nextFrame = [10, 0]
-    const thirdFrame = [3,4]
-    const isTenth = true
-    const expected = 16
-    const actual = score.scoreFrame(frame, nextFrame, thirdFrame, isTenth)
+    const frames = [[1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [10, 0, 6]]
+    const expected = 127
+    const actual = score.scoreFrame(frames)
     expect(actual).toBe(expected)
 })  
 
 test('scores a double strike in the 10th frame', () => {
-    const frame = [10, 10, 6]
-    const nextFrame = [10, 0]
-    const thirdFrame = [3,4]
-    const isTenth = true
-    const expected = 26
-    const actual = score.scoreFrame(frame, nextFrame, thirdFrame, isTenth)
+    const frames = [[1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [10, 10, 6]]
+    const expected = 137
+    const actual = score.scoreFrame(frames)
     expect(actual).toBe(expected)
 }) 
